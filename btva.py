c5fb2f2a619e35cf601cbd9b9cd24f561105b291
Kasra <kasra.ebrhm@gmail.com>
2025-02-21 15:34:35 +0100

    Completed borda strategic voting for btva

--- btva.py
+++ btva.py
@@ -2,6 +2,7 @@ import numpy as np
 import random
 from happiness import *
 import math
+import json
 
 class BTVA:
     def __init__(self, voting_scheme, preference_matrix):
@@ -108,72 +109,225 @@ class BTVA:
             self.risk_of_strategic_voting = np.sum(strategic_voting_incentives) / self.num_voters
 
         elif self.voting_scheme == 'borda':
-            # election_ranking, votes = election_result 
             strategic_voting_incentives = np.zeros(self.num_voters)
             winner  = election_ranking[0]
-            contenders = election_ranking[1:]
+            strategies = ['bullet', 'compromise', 'bury'] 
 
+            best_strategic_scenarios = [None] * self.num_voters
             for voter in range(self.num_voters):
-                voter_preference = self.preference_matrix[:, voter]                
-                # in borda any change can potentially increase happiness, so every alternative is a contender
-                for contender in contenders:
-                    # 1st strategy: bullet voting
-                    strategic_preference = np.full_like(voter_preference, -1)
-                    strategic_preference[0] = contender
-                    strategic_preference_matrix = np.ndarray.copy(self.preference_matrix)
-                    strategic_preference_matrix[:, voter] = strategic_preference
-                    btva_strategic = BTVA('borda', strategic_preference_matrix)
-                    new_election_ranking, new_votes = btva_strategic.run_non_strategic_election()
-                    
-                    new_happinesses = self.calc_happinesses(self.preference_matrix, new_election_ranking, exp_decay_borda_style_happiness)
+                voter_preference = self.preference_matrix[:, voter]  
+                original_happiness = self.non_strategic_happinesses[voter]
+                
+                voter_max_bullet_happiness = -1
+                voter_max_compromise_happiness = -1
+                voter_max_bury_happiness = -1
 
-                    if new_happinesses[voter] > self.non_strategic_happinesses[voter]:
-                        print(f'Voter {voter} strategic voting >>>')
-                        print('New Preference Matrix')
-                        print(strategic_preference_matrix)
-                        print('New Election Ranking', new_election_ranking)
-                        print('New Election Scores ', new_votes)
-                        print("Original Voters' Happiness", np.array2string(self.non_strategic_happinesses, formatter={'float_kind': lambda x: f"{x:.2f}"}))
-                        print("Updated Voters' Happiness ", np.array2string(new_happinesses, formatter={'float_kind': lambda x: f"{x:.2f}"}))
-                        print()
-                    
+                for strategy in strategies:
+
+                    #BULLET
+                    if strategy == 'bullet':
+                        best_bullet_scenarios = [None] * self.num_voters
+
+                        for contender in range(self.num_alternatives):
+                            strategic_preference = np.full_like(voter_preference, -1)
+                            strategic_preference[0] = contender
+                            strategic_preference_matrix = np.ndarray.copy(self.preference_matrix)
+                            strategic_preference_matrix[:, voter] = strategic_preference
+                            btva_strategic = BTVA('borda', strategic_preference_matrix)
+                            new_election_ranking, new_votes = btva_strategic.run_non_strategic_election()
+                            
+                            new_happinesses = self.calc_happinesses(self.preference_matrix, new_election_ranking, exp_decay_borda_style_happiness)
+
+                            if (new_happinesses[voter] > original_happiness) and (new_happinesses[voter] > voter_max_bullet_happiness):
+                                voter_max_bullet_happiness = new_happinesses[voter]
+                                
+                                best_bullet_scenarios[voter] = {
+                                    'strategy': 'bullet',
+                                    'strategic preference matrix': strategic_preference_matrix,
+                                    'new election ranking': new_election_ranking,
+                                    'new votes': new_votes,
+                                    'new happinesses': new_happinesses,
+                                    'voter original happiness': original_happiness,
+                                    'voter strategic happiness': voter_max_bullet_happiness
+                                }
+
+                        # # PRINTING BULLET SCENARIOS
+                        # if best_bullet_scenarios[voter]:
+                        #     print(f'VOTER {voter} STRATEGIC VOTING >>>')
+                        #     print(f'{voter_preference} -> {best_bullet_scenarios[voter]['strategic preference matrix'][:, voter]}')
+                        #     for key, value in best_bullet_scenarios[voter].items():
+                        #         if key == 'strategic preference matrix':
+                        #             print(key, value, sep='\n')
+                        #         else:
+                        #             print(f'{key}: {value}')
+                        #     print()
+
+                    #COMPROMISE
+                    elif strategy == 'compromise':
+                        best_compromise_scenarios = [None] * self.num_voters
+
+                        for contender in election_ranking[1:]:
+                            # Skip if the contender is already the voter's first preference, since compromising it wouldn't change anything
+                            if contender == voter_preference[0]:
+                                continue
+                            
+                            original_index = np.where(voter_preference == contender)[0][0]
+
+                            for i in range(1, original_index + 1):
+                                strategic_preference = np.copy(voter_preference)
+                                strategic_preference = np.delete(strategic_preference, original_index)
+                                # sliding up the contender 1 place at a time
+                                strategic_preference = np.insert(strategic_preference, original_index - i, contender)
+                                
+                                strategic_preference_matrix = np.copy(self.preference_matrix)
+                                strategic_preference_matrix[:, voter] = strategic_preference
+                                
+                                btva_strategic = BTVA('borda', strategic_preference_matrix)
+                                new_election_ranking, new_votes = btva_strategic.run_non_strategic_election()
+                                new_happinesses = self.calc_happinesses(self.preference_matrix, new_election_ranking, exp_decay_borda_style_happiness)
+                                
+                                # Check if this strategic move increases the voter's happiness
+                                if (new_happinesses[voter] > original_happiness) and (new_happinesses[voter] > voter_max_compromise_happiness):
+                                    
+                                    voter_max_compromise_happiness = new_happinesses[voter]
+                                    
+                                    best_compromise_scenarios[voter] = {
+                                        'strategy': 'compromise',
+                                        'strategic preference matrix': strategic_preference_matrix,
+                                        'new election ranking': new_election_ranking,
+                                        'new votes': new_votes,
+                                        'new happinesses': new_happinesses,
+                                        'voter original happiness': original_happiness,
+                                        'voter strategic happiness': voter_max_compromise_happiness
+                                    }
                         
-                        if strategic_voting_incentives[voter] == 0:
-                            strategic_voting_incentives[voter] = 1
+                        # # PRINTING COMPOROMISE SCENARIOS
+                        # if best_compromise_scenarios[voter]:
+                        #     print(f'VOTER {voter} STRATEGIC VOTING >>>')
+                        #     print(f'{voter_preference} -> {best_compromise_scenarios[voter]['strategic preference matrix'][:, voter]}')
+                        #     for key, value in best_compromise_scenarios[voter].items():
+                        #         if key == 'strategic preference matrix':
+                        #             print(key, value, sep='\n')
+                        #         else:
+                        #             print(f'{key}: {value}')
+                        #     print()
 
-            self.risk_of_strategic_voting = np.sum(strategic_voting_incentives) / self.num_voters
+                    #BURY
+                    elif strategy == 'bury':
+                        best_bury_scenarios = [None] * self.num_voters
 
-            # compromise
-            # bury
+                        for contender in election_ranking[1:]:
+                            # Skip if the contender is already the last preference, since burying it wouldn't change anything
+                            if contender == voter_preference[-1]:
+                                continue
+                            
+                            original_index = np.where(voter_preference == contender)[0][0]
 
-        return strategic_voting_incentives
+                            for i in range(1, self.num_alternatives - original_index):
+                                strategic_preference = np.copy(voter_preference)
+                                strategic_preference = np.delete(strategic_preference, original_index)
+                                strategic_preference = np.insert(strategic_preference, original_index + i, contender)
 
+                                strategic_preference_matrix = np.copy(self.preference_matrix)
+                                strategic_preference_matrix[:, voter] = strategic_preference
+                                                    
+                                btva_strategic = BTVA('borda', strategic_preference_matrix)
+                                new_election_ranking, new_votes = btva_strategic.run_non_strategic_election()
+                                new_happinesses = self.calc_happinesses(self.preference_matrix, new_election_ranking, exp_decay_borda_style_happiness)
+                                
+                                # Check if this strategic move increases the voter's happiness
+                                if (new_happinesses[voter] > original_happiness) and (new_happinesses[voter] > voter_max_bury_happiness):
+                                    
+                                    voter_max_bury_happiness = new_happinesses[voter]
+                                    
+                                    best_bury_scenarios[voter] = {
+                                        'strategy': 'bury', 
+                                        'strategic preference matrix': strategic_preference_matrix,
+                                        'new election ranking': new_election_ranking,
+                                        'new votes': new_votes,
+                                        'new happinesses': new_happinesses,
+                                        'voter original happiness': original_happiness,
+                                        'voter strategic happiness': voter_max_bury_happiness
+                                    }
+
+                        # # PRINTING BURY SCENARIOS    
+                        # if best_bury_scenarios[voter]:
+                        #     print(f'VOTER {voter} STRATEGIC VOTING >>>')
+                        #     print(f'{voter_preference} -> {best_bury_scenarios[voter]['strategic preference matrix'][:, voter]}')
+                        #     for key, value in best_bury_scenarios[voter].items():
+                        #         if key == 'strategic preference matrix':
+                        #             print(key, value, sep='\n')
+                        #         else:
+                        #             print(f'{key}: {value}')
+                        #     print()
+
+                # Finding the best strategic scenarios
+                strategic_scenarios = [best_bullet_scenarios[voter], best_compromise_scenarios[voter], best_bury_scenarios[voter]]
+                if all(value is None for value in strategic_scenarios):
+                    # let best_strategic_scenarios[voter] remain None
+                    pass
+                else:
+                    max_strategic_happiness = 0
+                    for scenario in strategic_scenarios:
+                        if scenario and (scenario['voter strategic happiness'] > max_strategic_happiness):
+                            best_strategic_scenarios[voter] = scenario
+
+        return best_strategic_scenarios
+    
+## FUNCTION TO GENERATE RANDOM PREFERENCE MATRICES   
+def generate_random_preferences_matrix(num_alternatives, num_voters):
+    return np.array([np.random.permutation(num_alternatives) for _ in range(num_voters)]).T
 
 pref_matrix = np.array([
-    [0, 1, 3, 0, 2, 3, 2, 3],
-    [2, 0, 0, 1, 4, 4, 4, 2],
-    [1, 3, 2, 2, 1, 0, 0, 1],
-    [3, 4, 4, 3, 3, 1, 1, 4],
-    [4, 2, 1, 4, 0, 2, 3, 0]
+    [1, 1, 3, 3, 2, 2, 3, 2],
+    [4, 3, 2, 0, 0, 3, 0, 3],
+    [3, 0, 4, 4, 1, 4, 4, 0],
+    [0, 4, 1, 2, 4, 1, 2, 4],
+    [2, 2, 0, 1, 3, 0, 1, 1]
 ])
 
-# Example Usage
-btva = BTVA('borda', pref_matrix)
+random_matrix = generate_random_preferences_matrix(num_alternatives=5, num_voters=8)
 
-# Non-strategic election
+# Example Usage
+btva = BTVA('borda', random_matrix)
+
+## Non-strategic election
 election_result = btva.run_non_strategic_election()
 election_ranking, votes = election_result
-happinesses = btva.calc_happinesses(pref_matrix, election_ranking, exp_decay_borda_style_happiness)
+happinesses = btva.calc_happinesses(random_matrix, election_ranking, exp_decay_borda_style_happiness)
 btva.non_strategic_happinesses = happinesses
 print()
-print('Non-strategic Election >>>')
+print(random_matrix)
+print()
+print(f'NON-STRATEGIC ELECTION ({btva.voting_scheme.upper()}) >>>')
 print('Winner', election_ranking[0])
 print('Election Ranking', election_ranking)
 print('Election Scores ', votes)
-print("Voters' Happinesses ", np.array2string(happinesses, formatter={'float_kind': lambda x: f"{x:.2f}"}))
+print("Voters' Happiness ", np.array2string(happinesses, formatter={'float_kind': lambda x: f"{x:.2f}"}))
 print()
 
-# Strategic election
-strategic_voting_incentives = btva.run_strategic_voting(election_result)
-print('Risk of Strategic Voting:', btva.risk_of_strategic_voting)
-print('Potential Strategic Voters:', strategic_voting_incentives.astype(int))
+## Strategic election
+best_strategic_scenarios = btva.run_strategic_voting(election_result)
+# PRINTING BEST STRATEGIC SCENARIOS    
+for voter in range(btva.num_voters):
+    if best_strategic_scenarios[voter]:
+        print(f'VOTER {voter} {best_strategic_scenarios[voter]['strategy'].upper()}ING >>>')
+        print(f'{btva.preference_matrix[:, voter]} -> {best_strategic_scenarios[voter]['strategic preference matrix'][:, voter]}')
+        for key, value in best_strategic_scenarios[voter].items():
+            if key == 'strategic preference matrix':
+                print(key, value, sep='\n')
+            elif key == 'strategy':
+                pass
+            else:
+                print(f'{key}: {value}')
+        print()
+
+voter_strategic_gains = np.zeros(btva.num_voters)
+for voter in range(btva.num_voters):
+    if not best_strategic_scenarios[voter]:
+        pass
+    else:
+        voter_strategic_gains[voter] = (best_strategic_scenarios[voter]['voter strategic happiness'] - best_strategic_scenarios[voter]['voter original happiness']) / best_strategic_scenarios[voter]['voter strategic happiness']
+
+print('VOTER STRATEGIC HAPPINESS GAINS:')
+print(np.array2string(voter_strategic_gains * 100, formatter={'float_kind': lambda x: f"{x:.0f}%"}))
